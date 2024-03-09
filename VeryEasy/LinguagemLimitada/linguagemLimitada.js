44d97d4f3b265783ca239fefbfe4bb9273e338ca
Magno <magno.alefb@gmail.com>
2024-03-08 22:59:33 -0300

    ultimo commit antes das ferias

---
+++ VeryEasy/LinguagemLimitada/linguagemLimitada.js
@@ -0,0 +1,9 @@
+//minha solucao
+function linguagemLimitada(numbers){
+ const reverse = numbers.reverse()
+return reverse
+}
+
+console.log(linguagemLimitada([0,9,6,8,9,1,5,7]))
+
+//solucao professor
