5b9f2ff258e1526b3ba2cd669394af479f5c7c26
Magno <magno.alefb@gmail.com>
2024-03-25 08:01:20 -0300

    linguagem limitada

--- VeryEasy/LinguagemLimitada/linguagemLimitada.js
+++ VeryEasy/LinguagemLimitada/linguagemLimitada.js
@@ -7,3 +7,15 @@ return reverse
 console.log(linguagemLimitada([0,9,6,8,9,1,5,7]))
 
 //solucao professor
+
+function linguagemLimitadaProfessor(numbers){
+    const revesedArray = []
+for(let i = 0 ; i < numbers.length ; i++){
+    revesedArray[i] = numbers[numbers.length - 1- i]
+}
+
+return revesedArray
+   }
+
+console.log(linguagemLimitadaProfessor([0,9,6,8,9,1,5,7]))
+console.log(linguagemLimitadaProfessor([true,false,false,false,true , true]))
