35418661515c59a5ccadfab7e41196d9ff5c5ca2
Magno <magno.alefb@gmail.com>
2024-04-04 19:10:47 -0300

    txt do exercicio final

--- VeryEasy/HistoriaEscondida1/historiaEscondida1.js
+++ VeryEasy/HistoriaEscondida1/historiaEscondida1.js
@@ -25,3 +25,7 @@ function highestLetter(str) {
     const sortedLetters = str.toLowerCase().split('').sort()
     return sortedLetters[sortedLetters.length - 1]
   }
+console.log(highestLetter('Lorem ipsum dolore sec avanti'))
+console.log(highestLetter('Hello'))
+console.log(highestLetter('May the force be with you'))
+console.log(highestLetter('Its over nine thousand'))
